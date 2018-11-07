909cfc1dfb4b8a3d4bcd7b901bb5ffa287ad4c89
Waqar-107 <waqar.hassan866@gmail.com>
2018-11-07 23:18:01 +0600

    added timer

---
+++ Miscellaneous/executionTimeDetermination.cpp
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+/***from dust i have come, dust i will be***/
+
+#include<algorithm>
+#include<cmath>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<functional>
+#include<iomanip>
+#include<iostream>
+#include<map>
+#include<numeric>
+#include<queue>
+#include<set>
+#include<string>
+#include<utility>
+#include<vector>
+
+typedef long long int ll;
+
+#define dbg printf("in\n");
+#define nl printf("\n");
+#define inf 10000000
+
+using namespace std;
+
+int main()
+{
+	//freopen("in2.txt", "r", stdin);
+
+	int i, j, k;
+	int n, m;
+
+	//----------------------------------------------------------------------
+	clock_t begin = clock();
+	
+	//the task whose time needs to be measured
+
+	clock_t end = clock();
+
+	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+	cout << time_spent << endl;
+	//----------------------------------------------------------------------
+
+	return 0;
+}
