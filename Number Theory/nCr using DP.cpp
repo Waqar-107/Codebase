be6ffbf107c3b1c037ebdaee677d5c7ad33cd448
Waqar-107 <waqar.hassan866@gmail.com>
2019-03-15 16:34:23 +0600

    bug fixed in ncr

--- Number Theory/nCr using DP.cpp
+++ Number Theory/nCr using DP.cpp
@@ -3,26 +3,10 @@
 #include<bits/stdc++.h>
 
 typedef long long int ll;
-typedef unsigned long long int ull;
 
 #define dbg printf("in\n")
 #define nl printf("\n")
 #define N 510
-#define inf 1e6
-
-#define sf(n) scanf("%d", &n)
-#define sff(n,m) scanf("%d%d",&n,&m)
-#define sfl(n) scanf("%I64d", &n)
-#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
-
-#define pf(n) printf("%d",n)
-#define pff(n,m) printf("%d %d",n,m)
-#define pffl(n,m) printf("%I64d %I64d",n,m)
-#define pfl(n) printf("%I64d",n)
-#define pfs(s) printf("%s",s)
-
-#define pb push_back
-#define pp pair<int, int>
 
 using namespace std;
 
@@ -32,6 +16,9 @@ ll nCr(int n, int r)
 {
     if(a[n][r] != -1)
         return a[n][r];
+	
+	if(r == 0)
+        a[n][r] = 1;
 
     if(n == r)
         a[n][r] = 1;
@@ -47,8 +34,6 @@ ll nCr(int n, int r)
 
 int main()
 {
-    freopen("in.txt", "r", stdin);
-
     int i, j, k;
     int n, m;
 
@@ -57,8 +42,6 @@ int main()
         for(j = 0; j < N; j++)
             a[i][j] = -1;
     }
-
-    cout<<nCr(4,2);
 
 
     return 0;
